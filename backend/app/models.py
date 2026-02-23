ff9a9c9b6e9a045c42cf32d11b16cd460b594c46
sHanant <sharma0420anant@gmail.com>
2026-02-23 09:53:38 +0530

    first push

---
+++ backend/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Patient(Base):
+    __tablename__ = "patients"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    age = Column(Integer)
+    disease = Column(String)
