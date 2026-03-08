6d5a049915a13d28bc8d286e5b75302086506185
sHanant <sharma0420anant@gmail.com>
2026-03-08 20:35:26 +0530

    hjn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {useState} from "react"
+import React, {useState} from "react"
 
 import Dashboard from "./pages/dashboard/dashboard"
 import WorkoutPlans from "./pages/workouts/workoutplan"
